49e07da65678132b1ad98660bce632fca12bf854
Kashan Ali Qazi <kashan.qazi02@utexas.edu>
2016-12-04 13:25:51 -0600

    initialized the files

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDdM3dm3A_ChXvGA7G4CYwhG3Nv4gVk1eE",
+    authDomain: "trainschedule-5714d.firebaseapp.com",
+    databaseURL: "https://trainschedule-5714d.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "1027151791440"
+};
+firebase.initializeApp(config);
+
+$(document).ready(function{
+
+
+
+
+});
